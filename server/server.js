86547c5d79253d3dc13b4ea4abbb4026c82e8676
Alec Jandorek <ajandorek@gmail.com>
2018-01-19 15:31:49 -0600

    Added beginnings of room functionality

--- server/server.js
+++ server/server.js
@@ -16,15 +16,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-
-  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-
   socket.on('join', (params, callback) => {
     if (!isRealString(params.name) || !isRealString(params.room)) {
       callback('Name and room are required');
     }
 
+    socket.join(params.room);
+    // To leave a room
+    // socket.leave(params.room);
+
+    // io.emit -> io.to(params.room).emit
+    // socket.broadcast.emit -> socket.brodcast.to(params.room).emit
+    //socket.emit
+
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+
+    socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined.`));
     callback();
   });
 
